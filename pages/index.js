66803bb1d510742b8130ca7e38f1dc3f20f520cc
Tormod <tormod.flesjo@gmail.com>
2023-06-25 02:56:42 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,46 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [horsePosition, setHorsePosition] = useState(0);
+
+  const handleClick = () => {
+    const newIntervalId = setInterval(() => {
+      let random = Math.random() * 5 + 2;
+      setHorsePosition((prevPosition) => prevPosition - random);
+    }, 80);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Pusteklem</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <span>Pusteklem!</span>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>🫂</p>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+      <footer style={{ position: "relative" }}>
+        <p>
+          Powered by{" "}
+          <span
+            onClick={handleClick}
+            style={{
+              cursor: "pointer",
+              position: "absolute",
+              transform: `translateX(${horsePosition}px)`,
+            }}
+          >
+            🐎
+          </span>
+        </p>
       </footer>
 
       <style jsx>{`
@@ -69,6 +52,14 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+        main p {
+          text-decoration: none;
+          color: inherit;
+          font-size: 10rem;
+        }
+        main span {
+          color: #0070f3;
+        }
         footer {
           width: 100%;
           height: 100px;
@@ -76,24 +67,6 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
@@ -111,5 +84,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
