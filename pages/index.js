318cec39b0a5a30816cfa62d233cc88467ca15b4
Tormod <tormod.flesjo@gmail.com>
2023-08-02 22:26:50 +0200

    quick commit

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ import data from "../data/parts.json";
 
 export default function Home() {
   const [easterEgg, setEasterEgg] = useState(false);
-  const title = easterEgg
-    ? "Slutt å trykk overalt!"
-    : "How to perform a Pusteklem!";
+  const easterEggAnimation = easterEgg ? "animate-spin" : "";
   return (
     <>
       <Head>
@@ -16,19 +14,24 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='p-2 flex flex-col justify-center items-center w-full bg-gray-100'>
+      <main className='p-2 flex flex-col justify-center items-center w-full bg-gray-100 '>
         <div
           style={{ maxWidth: "750px" }}
           className='w-full sm:w-5/6 md:w-3/4 lg:w-2/3 xl:w-7/12 2xl:w-6/12 mt-8 mb-10 md:mb-14 '
         >
           <section className='mb-12 mt-6 bg-white rounded-md py-12 pl-6  items-center'>
-            <h1 className=' text-3xl font-semibold text-center'>
-              {title}
+            <h1 className=' text-3xl font-semibold text-center relative'>
+              How to perform a Pusteklem!
               <span
-                className='cursor-default'
-                onClick={() => setEasterEgg(true)}
+                className={`cursor-default absolute ${easterEggAnimation}`}
+                onClick={() => {
+                  setEasterEgg(true);
+                  setTimeout(() => {
+                    setEasterEgg(false);
+                  }, 1000);
+                }}
               >
-                {easterEgg ? <span> {"😅"} </span> : <span>{"🫂"}</span>}
+                {"🫂"}
               </span>
             </h1>
             <p className='text-xs mt-4 w-1/3'>
